f11d58b3eb9a0ef43c7937e6cae3ada0eb95a4e5
Steve-Dusty <kuantingk2@gmail.com>
2021-03-04 22:43:06 -0800

    Still need JSON fixes for withdraw

--- account.py
+++ account.py
@@ -9,10 +9,22 @@ class Account():
         self.withdraw = 0
     
     def deposits(self, deposit):
-        self.balance += deposit
+        with open("accounts.json", "w") as f:
+            deposit_string = {"deposit":deposit}
+            json.dump(deposit_string, f, indent=4)
 
     def withdraws(self, withdraw):
-        self.balance -= withdraw
-    def get_balance(self):
-        return self.balance
+        with open("accounts.json", "r") as f:
+            deposit = json.load(f)
+        deposit["deposit"] -= withdraw
+        with open("accounts.json", "w") as f:
+            json.dump(deposit, f)
 
+    def get_balance(self):
+        with open("accounts.json", "r") as f:
+            balance = json.load(f)
+            return balance["deposit"]
+    
+
+
+    
--- bank.py
+++ bank.py
@@ -4,23 +4,28 @@ import json
 import account 
 
 
-user = account.Account()
-while True:
-    choices = input("Select: (1) Deposit (2) Withdraw (3) Get balance ")
 
-    if choices == 1: 
+user = account.Account()
+
+
+while True:
+    choices = input("Select: (1) Deposit (2) Withdraw (3) Get balance: ")
+
+    if choices == "1": 
         deposit = float(input("Input the amount: "))
         user.deposits(deposit)
         print(f"Your new balance is ${user.get_balance()}")
 
-    elif choices == 2:
+    elif choices == "2":
         withdraw = float(input("Input the amount: "))
         user.withdraws(withdraw)
         print(f"Your new balance is ${user.get_balance()}")
 
-    elif choices == 3:
+    elif choices == "3":
         print(f"Your balance is ${user.get_balance()}")
+        
 
     elif choices == "q":
         print("You've been signed out")
         break
+
